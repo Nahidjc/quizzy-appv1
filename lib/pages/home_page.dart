bdebf0b8108b0ad403987ee61ba2e7931217b553
alamin062 <alaminrahman0106@gmail.com>
2023-06-21 11:34:30 +0600

    home page design

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:quizzy/components/bottom-navigation.dart';
+import 'package:quizzy/components/categories.dart';
+import 'package:quizzy/components/header.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
-
   @override
   State<HomePage> createState() => _HomePageState();
 }
@@ -11,9 +12,19 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Column(),
-      bottomNavigationBar: BottomNav(),
-    );
+    return Scaffold(
+        appBar: const MyAppBar(),
+        body: Column(children: [
+          Container(
+              alignment: Alignment.centerLeft,
+              margin: const EdgeInsets.fromLTRB(15.0, 20.0, 15, 0.0),
+              child: const Text("Competitions Categories",
+                  textAlign: TextAlign.start,
+                  style:
+                      TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold))),
+          Expanded(child: Categories())
+        ]),
+         bottomNavigationBar: const BottomNav(),
+        );
   }
 }
