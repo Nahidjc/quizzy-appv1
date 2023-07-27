548cadfbf66d44f3cac9a6baa93d896f086779b9
Nahid Hasan <nahidjc80@gmail.com>
2023-07-27 09:52:12 +0600

    logout issue fixed:

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -1,66 +1,98 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:quizzy/pages/home_page.dart';
+import 'package:quizzy/pages/login_page.dart';
+import 'package:quizzy/provider/login_provider.dart';
 
-class ProfilePage extends StatelessWidget {
-  final String username = "John Doe";
-  final int totalScore = 550;
-
-  const ProfilePage({super.key});
+class ProfilePage extends StatefulWidget {
+  const ProfilePage({Key? key}) : super(key: key);
 
   @override
+  State<ProfilePage> createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+  @override
   Widget build(BuildContext context) {
+    final user = Provider.of<AuthProvider>(context);
+
+    if (!user.isAuthenticated) {
+      return const LoginPage();
+    }
+
     return Scaffold(
       appBar: AppBar(
-        title:
-            const Text('User Profile', style: TextStyle(color: Colors.white)),
+        title: const Text(
+          'User Profile',
+          style: TextStyle(color: Colors.white),
+        ),
         toolbarHeight: 120.0,
         centerTitle: true,
         automaticallyImplyLeading: false,
-        leading: null,
         backgroundColor: Colors.purple,
         shape: const RoundedRectangleBorder(
           borderRadius: BorderRadius.vertical(
             bottom: Radius.circular(20),
           ),
         ),
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.of(context).pop(); // Navigate back to the previous page
+          },
+        ),
       ),
-      body: Stack(
-        children: [
-          Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  username,
-                  style: const TextStyle(
-                      fontSize: 24, fontWeight: FontWeight.bold),
-                ),
-                const SizedBox(height: 8),
-                Text(
-                  'Total Score: $totalScore',
-                  style: const TextStyle(fontSize: 18, color: Colors.grey),
-                ),
-                const SizedBox(height: 24),
-                ElevatedButton(
-                  style: ButtonStyle(
-                    backgroundColor:
-                        MaterialStateProperty.all<Color>(Colors.purple),
+      body: SafeArea(
+        child: Stack(
+          children: [
+            Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    user.name,
+                    style: const TextStyle(
+                        fontSize: 24, fontWeight: FontWeight.bold),
                   ),
-                  onPressed: () {},
-                  child: const Text('Logout',
-                      style: TextStyle(color: Colors.white, fontSize: 18.0)),
-                ),
-              ],
+                  const SizedBox(height: 8),
+                  Text(
+                    'Total Score: ${user.coin}',
+                    style: const TextStyle(fontSize: 18, color: Colors.grey),
+                  ),
+                  const SizedBox(height: 24),
+                  TextButton.icon(
+                    style: TextButton.styleFrom(
+                      foregroundColor: Colors.white,
+                      backgroundColor: Colors.purple,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8.0),
+                      ),
+                    ),
+                    onPressed: () {
+                      if (user.isAuthenticated) {
+                        user.logout();
+                        Navigator.popUntil(context, (route) => route.isFirst);
+                      }
+                    },
+                    icon: const Icon(Icons.logout),
+                    label: const Text(
+                      'Logout',
+                      style: TextStyle(fontSize: 18.0),
+                    ),
+                  ),
+                ],
+              ),
             ),
-          ),
-          Positioned(
-            top: kToolbarHeight + 0.0,
-            left: MediaQuery.of(context).size.width / 2 - 80,
-            child: const CircleAvatar(
-              radius: 80,
-              backgroundImage: AssetImage("assets/images/avatar.png"),
+            Positioned(
+              top: kToolbarHeight + 0.0,
+              left: MediaQuery.of(context).size.width / 2 - 80,
+              child: const CircleAvatar(
+                radius: 80,
+                backgroundImage: AssetImage("assets/images/avatar.png"),
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:quizzy/pages/home_page.dart';
 import 'package:quizzy/pages/login_page.dart';
+import 'package:quizzy/pages/profile_page.dart';
 import 'package:quizzy/pages/signup_page.dart';
 import 'package:quizzy/provider/login_provider.dart';
 import 'package:quizzy/widget/authenticate_checker.dart';
@@ -11,6 +12,7 @@ class AppRoutes {
   static const String home = '/home';
   static const String login = '/login';
   static const String register = '/register';
+  static const String profile = '/profile';
 
   static Map<String, WidgetBuilder> routes = {
     home: (context) => AuthenticatedRoute(
@@ -19,5 +21,9 @@ class AppRoutes {
         ),
     login: (context) => const LoginPage(),
     register: (context) => const SignupPage(),
+    profile: (context) => AuthenticatedRoute(
+          page: const ProfilePage(),
+          isAuthenticated: Provider.of<AuthProvider>(context).isAuthenticated,
+        ),
   };
 }
